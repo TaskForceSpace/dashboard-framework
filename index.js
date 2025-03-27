07ce4c99ac0577549d0c8d067a30e31f761a973c
Karolis Dailidonnis <mail@karolisdailidonis.de>
2025-03-27 12:20:51 +0100

    add/dialog and spinner

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Components
 import Button from "./src/components/Button.vue";
 import Header from "./src/components/Header.vue";
+import Dialog from "./src/components/Dialog.vue";
 import HeaderData from "./src/components/HeaderData.vue";
 import HeaderEditor from "./src/components/HeaderEditor.vue";
 import Input from "./src/components/Input.vue";
@@ -8,6 +9,7 @@ import FormField from "./src/components/FormField.vue";
 import FormContainer from "./src/components/FormContainer.vue";
 import InfoContainer from "./src/components/InfoContainer.vue";
 import InfoRow from "./src/components/InfoRow.vue";
+import Spinner from "./src/components/Spinner.vue";
 import SideNavigation from "./src/components/SideNavigation.vue";
 
 // Views
@@ -18,6 +20,7 @@ export {
 	Input,
 	Header,
 	Button,
+	Dialog,
 	InfoContainer,
 	InfoRow,
 	SideNavigation,
@@ -25,6 +28,7 @@ export {
 	HeaderData,
 	FormField,
 	FormContainer,
+	Spinner,
 	SideNavView,
 	LoginView
 };
@@ -32,6 +36,7 @@ export {
 export default {
 	install(app) {
 		app.component("Button", Button);
+		app.component("Dialog", Dialog);
 		app.component("Input", Input);
 		app.component("SideNavigation", SideNavigation);
 		app.component("Header", Header);
@@ -40,6 +45,7 @@ export default {
 		app.component("FormField", FormField);
 		app.component("InfoContainer", InfoContainer);
 		app.component("InfoRow", InfoRow);
+		app.component("Spinner", Spinner);
 
 		app.component("SideNavView", SideNavView);
 		app.component("LoginView", LoginView);
---
+++ src/components/Dialog.vue
@@ -0,0 +1,121 @@
+<template>
+	<div v-if="isShow" class="dialog">
+		<div class="dialog__container">
+			<div v-if="!loading" class="dialog__content">
+				<div class="dialog__header">
+					<div class="dialog__close"><span class="dialog__close--action" @click="end()">X</span></div>
+					<slot name="header"></slot>
+				</div>
+
+				<div class="dialog__body">
+					<slot name="body"></slot>
+				</div>
+
+				<div class="dialog__footer">
+					<slot name="footer"></slot>
+				</div>
+			</div>
+
+			<div v-else class="dialog__loading">
+				<Spinner />
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import { ref, onMounted, watch } from 'vue'
+import Spinner from './Spinner.vue'
+
+const props = defineProps({
+	loading: {
+		type: Boolean,
+		default: false
+	},
+	show: {
+		type: Boolean,
+		default: true
+	}
+})
+
+watch(() => props.show, (newVal) => {
+	isShow.value = newVal;
+	if (newVal) {
+		document.body.style.overflow = 'hidden';
+		window.scrollTo(0, 0);
+
+	} else {
+		document.body.style.overflow = '';
+	}
+});
+
+const isShow = ref(false)
+
+const emit = defineEmits(['update:show']);
+
+function end() {
+	document.body.style.overflow = '';
+	isShow.value = false;
+	emit('update:show', isShow.value);
+}
+
+onMounted(
+	() => {
+		isShow.value = props.show;
+	}
+)
+</script>
+
+<style lang="scss" scoped>
+.dialog {
+	background-color: rgba(0, 0, 0, 0.603);
+	position: absolute;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	z-index: 9999;
+
+	&__container {
+		background-color: #fff;
+		border-radius: 12px;
+		width: 100%;
+		max-width: 500px;
+		padding: 15px;
+		margin: 15px;
+		box-sizing: border-box;
+		min-height: 200px;
+		display: flex;
+		flex-flow: column;
+		justify-content: start;
+	}
+
+	&__loading {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 200px;
+	}
+
+	&__close {
+		display: flex;
+		justify-content: end;
+
+		&--action {
+			background-color: rgba(192, 192, 192, 0.397);
+			font-size: 24px;
+			padding: 4px 13px;
+			border-radius: 100%;
+			cursor: pointer;
+
+			&:hover {
+				background-color: rgb(199, 199, 199);
+			}
+		}
+	}
+
+}
+</style>
